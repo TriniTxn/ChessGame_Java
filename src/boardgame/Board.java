55ad3d81411a9cf8d63b35d46efe5fb44f45203c
Giovanni dos Santos <rm550303@fiap.com.br>
2023-11-27 22:07:42 -0300

    chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -26,4 +26,11 @@ public class Board {
     public void setColumns(int columns) {
         this.columns = columns;
     }
+
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+    public Piece position(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
 }
